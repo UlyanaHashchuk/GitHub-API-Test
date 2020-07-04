6afc0c314971a077df27f75133b3b4a8678771c7
UlyanaHashchuk <ulyanahashchuk@gmail.com>
2020-07-04 17:38:10 +0200

    Updates

--- precache-manifest.ad6c98d0007ddbbbf072895e8897d968.js
+++ precache-manifest.ad6c98d0007ddbbbf072895e8897d968.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "59f38ee11d7baa0e3e182f101036fde1",
+    "revision": "09f0a9b57762e006a3eb6cd0bc4da03c",
     "url": "/GitHub-API-Test/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/GitHub-API-Test/static/js/2.3141db3f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4892413a1f146b55604b",
-    "url": "/GitHub-API-Test/static/js/main.8f1c1735.chunk.js"
+    "revision": "4a99628a35bf06328922",
+    "url": "/GitHub-API-Test/static/js/main.436a1999.chunk.js"
   },
   {
     "revision": "e291b410601d270d70cd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GitHub-API-Test/precache-manifest.1f822390d30a06abc291e8fde338368f.js"
+  "/GitHub-API-Test/precache-manifest.ad6c98d0007ddbbbf072895e8897d968.js"
 );
 
 self.addEventListener('message', (event) => {
