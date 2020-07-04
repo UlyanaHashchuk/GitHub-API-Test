60ae6a3b0d45708093078a61b4daf2f60a651b84
UlyanaHashchuk <ulyanahashchuk@gmail.com>
2020-07-04 15:24:09 +0200

    Updates

---
+++ precache-manifest.2d4b90120a9837c46fda9a7e0c45a3cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78661361704f634d698777547835eaf5",
+    "url": "/GitHub-API-Test/index.html"
+  },
+  {
+    "revision": "79fd8ff0648c6bf5077a",
+    "url": "/GitHub-API-Test/static/js/2.3141db3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GitHub-API-Test/static/js/2.3141db3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c31d614d67bb552c2a",
+    "url": "/GitHub-API-Test/static/js/main.9c47e19a.chunk.js"
+  },
+  {
+    "revision": "e291b410601d270d70cd",
+    "url": "/GitHub-API-Test/static/js/runtime-main.6e47b24c.js"
+  }
+]);
